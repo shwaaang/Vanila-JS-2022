b2174a35dd9bd47134d78170c60581554cf66826
shwaaang <gtg914@naver.com>
2022-10-26 19:10:25 +0900

    momentum-basics-completed

--- js/background.js
+++ js/background.js
@@ -1,11 +1,13 @@
+const body = document.querySelector("body");
+
 const images=["0.jpg","1.jpg","2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
 const bgImage = document.createElement("img");
-
 bgImage.src=`img/${chosenImage}`;
-
-console.log(bgImage);
-
+bgImage.classList.add("bgImage")
 document.body.appendChild(bgImage);
+body.backgroundImage=`img/${chosenImage}`;
+
+
